ff56f30eca407ba767b85e99d150ea490558258d
silvaokindo <okindosilva@gmail.com>
2025-11-19 20:59:32 +0300

    configured cors

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const config = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+    WHITELISTED_ORIGINS: ["http://localhost"]
 }
 
 export default config
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,25 @@
 import express from 'express'
 import config from '@/config'
+import cors, {  } from 'cors'
+import type { CorsOptions } from 'cors'
 
 const app = express()
+
+// configure cors
+
+const corsOptions: CorsOptions = {
+    origin(origin, callback) {
+        if(config.NODE_ENV === 'development' || !origin || config.WHITELISTED_ORIGINS.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error(`CORS ERROR: ${origin} is not allowed by cors`), false)
+        }
+    }
+}
+
+// middlewares
+
+app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
     res.status(200).json({message: "server is up"})
@@ -10,3 +28,4 @@ app.get('/', (req, res) => {
 app.listen(config.PORT, ()=> {
     console.log("server running on port 3000")
 })
+

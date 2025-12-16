9dce437c35bfe3dec16b23b1cd3a7bfc5491dbc8
silvaokindo <okindosilva@gmail.com>
2025-12-16 20:46:41 +0300

    updated delete current user controller to delete both the blogs and images in cloudinary

--- src/controllers/v1/users/delete_current_user.ts
+++ src/controllers/v1/users/delete_current_user.ts
@@ -1,14 +1,38 @@
 import User from '@/models/user';
+import Blog from '@/models/blog'
 import {Response, Request} from 'express'
+import {v2 as cloudinary} from 'cloudinary'
 
 import { logger } from "@/lib/logger"
+import { log } from 'console';
 
 export const deleteCurrentUser = async (req: Request, res: Response): Promise<void> => {
     try {
 
         const userId = req.userId
 
+        const user = await User.findById(userId).select('role').lean().exec()
+
+        if(!user) {
+            res.status(404).json({
+                message: "User not found"
+            })
+            return
+        }
+
+        if(user.role === "admin") {
+
+            const blogs = await Blog.find({author: userId}).select('banner.publicId').lean().exec()
+            const publicIds = blogs.map(({banner}) => banner.publicId)
+            await cloudinary.api.delete_resources(publicIds)
+            logger.info("deleted banners from cloudinary successfully")
+
+            await Blog.deleteMany({author: userId})
+            logger.info("Successfully deleted many blogs for", {userId})
+        }
+
         await User.deleteOne({_id: userId})
+        logger.info("Successfully deleted user", {userId})
         res.sendStatus(204)
 
     } catch (error) {
